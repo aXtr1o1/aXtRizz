4e0015f1a5f57f3dd76e42fb49f37f05ba5169cd
Praga3004 <pragadeswaran.m.s@gmail.com>
2025-02-14 01:40:27 +0530

    GET RIZZFIED

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -60,7 +60,7 @@ const ContextProvider = (props) => {
         // Prepare messages for backend request
         const updatedChatHistory = [...chatHistory, { role: "user", content: currentPrompt }];
         
-        const response = await fetch("http://134.209.150.173:5001/chat", {
+        const response = await fetch("https://rizz.axtr-product.tech/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ sessionId, userPrompt: currentPrompt, chatHistory: updatedChatHistory }),
