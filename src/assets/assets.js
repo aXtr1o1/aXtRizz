141e2e439211c943bad80398482847e97f941f11
Praga3004 <pragadeswaran.m.s@gmail.com>
2025-02-14 00:42:17 +0530

    frontEnd: SysMode

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -13,8 +13,8 @@ import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
-import favicon from './favicon.ico'
-import faviconlight from './Favicon-light.png'
+import favicon from './favicon.png'
+
 
 
 export const assets = {
@@ -34,6 +34,6 @@ export const assets = {
     send_icon,
     gemini_icon,
     favicon,
-    faviconlight
+  
 
 }
